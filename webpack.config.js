973894dcee8229c2eaee1077ea2c55ec405b5788
Juliana <aragaopintojuli@gmail.com>
2023-07-10 23:51:09 -0300

    now the giripoca is going to pew pew

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+import path from 'path';
+
+const config = {
+  mode: 'none',
+  target: 'node',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('.', 'dist'),
+    filename: 'pbfdb.js',
+    libraryTarget: 'commonjs2' // adiciona essa linha
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // aplique a regra para arquivos JS
+        exclude: /node_modules/, // exclua a pasta node_modules
+        use: {
+          loader: 'babel-loader', // use o babel-loader para transpilar
+          options: {
+            presets: ['@babel/preset-env'] // use o preset @babel/preset-env
+          }
+        }
+      }
+    ]
+  }
+};
+
+export default config;

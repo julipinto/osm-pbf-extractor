a8aca1be94c3fabd7b1cf898d53cec0ecfe30cf9
Juliana <aragaopintojuli@gmail.com>
2023-07-20 23:15:06 -0300

    transaction methods

--- src/plugins/mysql/MysqlConnection.js
+++ src/plugins/mysql/MysqlConnection.js
@@ -31,7 +31,7 @@ class MysqlConnection {
         await populateConnection.query(`CREATE DATABASE ${this.database}`);
         await populateConnection.query(`CREATE TABLE \`${this.database}\`.\`nodes\` ( \`node_id\` bigint(64) NOT NULL, \`location\` POINT NOT NULL SRID 0, PRIMARY KEY (node_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4`);
         await populateConnection.query(`CREATE TABLE \`${this.database}\`.\`node_tags\` (\`node_id\` bigint(64) NOT NULL, \`tag_key\` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL default '', \`tag_value\` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL default '', PRIMARY KEY (\`node_id\`, \`tag_key\`), CONSTRAINT \`node_tags_ibfk_1\` FOREIGN KEY (\`node_id\`) REFERENCES \`nodes\` (\`node_id\`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;`);
-        await populateConnection.query(`CREATE TABLE \`${this.database}\`.\`ways\` (\`way_id\` bigint(64) NOT NULL, PRIMARY KEY (\`way_id\`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;`);
+        await populateConnection.query(`CREATE TABLE \`${this.database}\`.\`ways\` (\`way_id\` bigint(64) NOT NULL, \`way_line\` LINESTRING DEFAULT NULL SRID 0, PRIMARY KEY (\`way_id\`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;`);
         await populateConnection.query(`CREATE TABLE \`${this.database}\`.\`way_tags\` (\`way_id\` bigint(64) NOT NULL, \`tag_key\` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL default '', \`tag_value\` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL default '', PRIMARY KEY (\`way_id\`,\`tag_key\`), CONSTRAINT \`way_tags_ibfk_1\` FOREIGN KEY (\`way_id\`) REFERENCES \`ways\` (\`way_id\`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;`);
         await populateConnection.query(`CREATE TABLE \`${this.database}\`.\`way_nodes\` (\`way_id\` bigint(64) NOT NULL, \`node_id\` bigint(64) NOT NULL, \`sequence_index\` int(11) NOT NULL, PRIMARY KEY (\`way_id\`,\`sequence_index\`), CONSTRAINT \`way_nodes_ibfk_1\` FOREIGN KEY (\`way_id\`) REFERENCES \`ways\` (\`way_id\`), CONSTRAINT \`way_nodes_ibfk_2\` FOREIGN KEY (\`node_id\`) REFERENCES \`nodes\` (\`node_id\`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;`);
 
@@ -47,7 +47,7 @@ class MysqlConnection {
     try {
       this.#connection = await this.conectionWithTimeout({ timeout: this.connection_timeout, database: this.database });
 
-      console_connection.succeed(`Connectec to database ${this.database}`);
+      console_connection.succeed(`Connected to database ${this.database}`);
 
       await this.init();
     } catch (error) {
@@ -70,11 +70,20 @@ class MysqlConnection {
   }
 
   async query(query) {
-    try {
-      await this.#connection.query(query);
-    } finally {
-      await this.#connection.query('UNLOCK TABLES');
-    }
+    return this.#connection.query(query);
+
+  }
+
+  beginTransaction() {
+    return this.#connection.beginTransaction();
+  }
+
+  commit() {
+    return this.#connection.commit();
+  }
+
+  rollback() {
+    return this.#connection.rollback();
   }
 
   async conectionWithTimeout({ timeout, database }) {
